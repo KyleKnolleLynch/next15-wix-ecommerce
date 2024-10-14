ac4d0bceccd7f0c361884d63148c11bb07749cb3
kyle <kylelynch104@gmail.com>
2024-10-14 14:50:38 -0500

    add deps, dev deps, shadcn components

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+const config = {
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: false,
+  arrowParens: 'avoid',
+  tailwindAttributes: ['myClassList'],
+  plugins: ['prettier-plugin-tailwindcss'],
+}
+export default config
