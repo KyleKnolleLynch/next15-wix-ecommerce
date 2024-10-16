a00658d61dd7bdcb2e6cc7f5a7cf4ceb312630ee
kyle <kylelynch104@gmail.com>
2024-10-16 12:46:18 -0500

    Product component, fetch and display featured products collection

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,15 @@ import banner from '@/assets/banner.jpg'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import { ArrowRightIcon } from '@radix-ui/react-icons'
+import { delay } from '@/lib/utils'
+import { Suspense } from 'react'
+import { getWixClient } from '@/lib/wix-client.base'
+import Product from '@/components/Product'
 
 export default function Home() {
   return (
     <main className='mx-auto max-w-7xl space-y-10 px-5 py-10'>
-      <div className='flex items-center bg-secondary md:h-96'>
+      <section className='flex items-center bg-secondary md:h-96'>
         <div className='space-y-7 p-10 text-center md:w-1/2'>
           <h1 className='text-3xl font-bold md:text-4xl'>
             Lorem ipsum dolor sit amet.
@@ -22,15 +26,48 @@ export default function Home() {
             </Link>
           </Button>
         </div>
-        <div className='hidden h-full w-1/2 md:block'>
+        <div className='relative hidden h-full w-1/2 md:block'>
           <Image
             src={banner}
             alt='Modern Wearables banner'
             className='h-full object-cover'
           />
+          <div className='absolute inset-0 bg-gradient-to-r from-secondary via-transparent to-transparent' />
         </div>
-      </div>
+      </section>
+      <Suspense fallback={'Loading...'}>
+        <FeaturedProducts />
+      </Suspense>
     </main>
   )
 }
 
+async function FeaturedProducts() {
+  await delay(1000)
+
+  const wixClient = getWixClient()
+
+  const { collection } =
+    await wixClient.collections.getCollectionBySlug('featured-products')
+
+  if (!collection?._id) return null
+
+  const featuredProducts = await wixClient.products
+    .queryProducts()
+    .hasSome('collectionIds', [collection._id])
+    .descending('lastUpdated')
+    .find()
+
+  if (!featuredProducts.items.length) return null
+
+  return (
+    <section className='space-y-5'>
+      <h2 className='text-2xl font-bold'>Featured Products</h2>
+      <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+        {featuredProducts.items.map(product => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
+    </section>
+  )
+}
