ed280a8e9da82f502af4b9e0c2ebd03b199dd90c
kyle <kylelynch104@gmail.com>
2024-10-31 13:24:20 -0500

    optimistic updates with useUpdateCartItemQuantity hook

--- app/shopping-cart-button.tsx
+++ app/shopping-cart-button.tsx
@@ -2,8 +2,8 @@
 
 import { useState } from 'react'
 import { currentCart } from '@wix/ecom'
-import { ShoppingCartIcon } from 'lucide-react'
-import { useCart } from '@/hooks/cart'
+import { Loader2, ShoppingCartIcon } from 'lucide-react'
+import { useCart, useUpdateCartItemQuantity } from '@/hooks/cart'
 import { Button } from '@/components/ui/button'
 import {
   Sheet,
@@ -57,6 +57,26 @@ export default function ShoppingCartButton({
                 <ShoppingCartItem key={item._id} item={item} />
               ))}
             </ul>
+            {cartQuery.isPending && (
+              <Loader2 className='mx-auto animate-spin' />
+            )}
+            {cartQuery.error && (
+              <p className='text-destructive'>{cartQuery.error.message}</p>
+            )}
+            {!cartQuery.isPending && !cartQuery.data?.lineItems?.length && (
+              <div className='grid grow place-items-center text-center'>
+                <div className='space-y-1.5'>
+                  <p className='text-lg font-semibold'>Your cart is empty</p>
+                  <Link
+                    href='/shop'
+                    className='text-primary hover:underline'
+                    onClick={() => setSheetOpen(false)}
+                  >
+                    Start shopping
+                  </Link>
+                </div>
+              </div>
+            )}
             <pre>{JSON.stringify(cartQuery.data, null, 2)}</pre>
           </div>
           <div className='flex items-center justify-between gap-5'>
@@ -70,7 +90,12 @@ export default function ShoppingCartButton({
                 Shipping and taxes calculated at checkout
               </p>
             </div>
-            <Button size='lg'>Checkout</Button>
+            <Button
+              size='lg'
+              disabled={!totalCartQuantity || cartQuery.isFetching}
+            >
+              Checkout
+            </Button>
           </div>
         </SheetContent>
       </Sheet>
@@ -83,6 +108,12 @@ interface ShoppingCartItemProps {
 }
 
 function ShoppingCartItem({ item }: ShoppingCartItemProps) {
+  const updateQuantityMutation = useUpdateCartItemQuantity()
+
+  const productId = item._id
+
+  if (!productId) return null
+
   const slug = item.url?.split('/').pop()
 
   const quantityLimitReached =
@@ -107,11 +138,52 @@ function ShoppingCartItem({ item }: ShoppingCartItemProps) {
         </Link>
         {!!item.descriptionLines?.length && (
           <p>
-            {item.descriptionLines.map(
-              line => line.colorInfo?.translated || line.plainText?.translated,
-            ).join(', ')}
+            {item.descriptionLines
+              .map(
+                line =>
+                  line.colorInfo?.translated || line.plainText?.translated,
+              )
+              .join(', ')}
           </p>
         )}
+        <div className='flex items-center gap-2'>
+          {item.quantity} x {item.price?.formattedConvertedAmount}
+          {item.fullPrice && item.fullPrice.amount !== item.price?.amount && (
+            <span className='text-muted-foreground line-through'>
+              {item.fullPrice.formattedConvertedAmount}
+            </span>
+          )}
+        </div>
+        <div className='flex items-center gap-1.5'>
+          <Button
+            variant='outline'
+            size='sm'
+            disabled={item.quantity === 1}
+            onClick={() =>
+              updateQuantityMutation.mutate({
+                productId,
+                newQuantity: !item.quantity ? 0 : item.quantity - 1,
+              })
+            }
+          >
+            -
+          </Button>
+          <span>{item.quantity}</span>
+          <Button
+            variant='outline'
+            size='sm'
+            disabled={quantityLimitReached}
+            onClick={() =>
+              updateQuantityMutation.mutate({
+                productId,
+                newQuantity: !item.quantity ? 1 : item.quantity + 1,
+              })
+            }
+          >
+            +
+          </Button>
+          {quantityLimitReached && <span>Quantity limit reached</span>}
+        </div>
       </div>
     </li>
   )
--- hooks/cart.ts
+++ hooks/cart.ts
@@ -1,4 +1,5 @@
 import {
+  MutationKey,
   QueryKey,
   useMutation,
   useQuery,
@@ -6,7 +7,13 @@ import {
 } from '@tanstack/react-query'
 import { currentCart } from '@wix/ecom'
 import { wixBrowserClient } from '@/lib/wix.client.browser'
-import { addToCart, AddToCartValues, getCart } from '@/wix-api/cart'
+import {
+  addToCart,
+  AddToCartValues,
+  getCart,
+  updateCartItemQuantity,
+  UpdateCartItemQuantityValues,
+} from '@/wix-api/cart'
 import { useToast } from './use-toast'
 
 const queryKey: QueryKey = ['cart']
@@ -42,3 +49,45 @@ export function useAddItemToCart() {
   })
 }
 
+export function useUpdateCartItemQuantity() {
+  const queryClient = useQueryClient()
+
+  const { toast } = useToast()
+
+  const mutationKey: MutationKey = ['useUpdateCartItemQuantity']
+
+  return useMutation({
+    mutationKey,
+    mutationFn: (values: UpdateCartItemQuantityValues) =>
+      updateCartItemQuantity(wixBrowserClient, values),
+    onMutate: async ({ productId, newQuantity }) => {
+      await queryClient.cancelQueries({ queryKey })
+
+      const previousState = queryClient.getQueryData<currentCart.Cart>(queryKey)
+
+      queryClient.setQueryData<currentCart.Cart>(queryKey, oldData => ({
+        ...oldData,
+        lineItems: oldData?.lineItems?.map(lineItem =>
+          lineItem._id === productId
+            ? { ...lineItem, quantity: newQuantity }
+            : lineItem,
+        ),
+      }))
+
+      return { previousState }
+    },
+    onError(error, variables, context) {
+      queryClient.setQueryData(queryKey, context?.previousState)
+      console.error(error)
+      toast({
+        variant: 'destructive',
+        description: 'Something went wrong. Please try again.',
+      })
+    },
+    onSettled() {
+      if (queryClient.isMutating({ mutationKey }) === 1) {
+        queryClient.invalidateQueries({ queryKey })
+      }
+    },
+  })
+}
